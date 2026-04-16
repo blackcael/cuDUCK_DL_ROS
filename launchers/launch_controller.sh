2923a9bfb8a9602016d6d58687a92e23779b5177
Cael <blackcael@gmail.com>
2026-04-16 14:52:27 -0600

    bug fix maybe

--- launchers/launch_controller.sh
+++ launchers/launch_controller.sh
@@ -15,8 +15,24 @@ dt-launchfile-init
 # NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
+WAIT_FOR_JOYSTICK="${WAIT_FOR_JOYSTICK:-1}"
+WAIT_FOR_JOYSTICK_INTERVAL="${WAIT_FOR_JOYSTICK_INTERVAL:-1}"
+WAIT_FOR_JOYSTICK_PATTERN="${WAIT_FOR_JOYSTICK_PATTERN:-/dev/input/js*}"
+
+if [ "${WAIT_FOR_JOYSTICK}" = "1" ]; then
+    echo "[launch_controller] Waiting for joystick (${WAIT_FOR_JOYSTICK_PATTERN})..."
+    while true; do
+        if compgen -G "${WAIT_FOR_JOYSTICK_PATTERN}" > /dev/null; then
+            echo "[launch_controller] Joystick detected."
+            ls -l /dev/input || true
+            break
+        fi
+        sleep "${WAIT_FOR_JOYSTICK_INTERVAL}"
+    done
+fi
+
 # launching app
-roslaunch $HW_PACKAGE $HW_LAUNCH
+dt-exec roslaunch $HW_PACKAGE $HW_LAUNCH
 
 
 # ----------------------------------------------------------------------------
